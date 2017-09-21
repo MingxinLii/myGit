f8a83c27012f6cc45cd683c16d84a6eff2a42ce2
mingxin <mingxin@kooboo.com>
2017-09-21 18:19:38 +0800

    JS 4.5

--- scripts/changeImage.js
+++ scripts/changeImage.js
@@ -1,13 +1,22 @@
 /**
  * Created by mingxin on 2017/9/20.
  */
-  function changeImage(whichPic){
-    let source = whichPic.getAttribute("href");
-    let placeholder = document.getElementById("placeholder");
-    placeholder.setAttribute("src",source);
-  }
-   function countBodyChildren(){
-    let bodyElement = document.getElementsByTagName("body")[0];
-    alert(bodyElement.childNodes.length);  
-   }
-   window.onload = countBodyChildren;
+function changeImage(whichPic) {
+  let source = whichPic.getAttribute("href");
+  let title = whichPic.getAttribute("title");
+  let placeholder = document.getElementById("placeholder");
+  let desctiption = document.getElementById("description");
+  placeholder.setAttribute("src", source);
+  desctiption.firstChild.nodeValue = title;
+  // alert(desctiption.childNodes[0].nodeValue);
+
+}
+
+function countBodyChildren() {
+  let bodyElement = document.getElementsByTagName("body")[0];
+  // alert(bodyElement.childNodes.length);
+  // alert(bodyElement.nodeType);
+  
+}
+
+window.onload = countBodyChildren;

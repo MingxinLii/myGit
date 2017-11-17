3dce82b3a8abe2d22d40a350a8a66152b6a1b927
mingxin <mingxin@kooboo.com>
2017-11-17 18:28:52 +0800

    set up

---
+++ pengjing/pengjing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pengjing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The first C# program");
+            Console.ReadKey();
+        }
+    }
+}
